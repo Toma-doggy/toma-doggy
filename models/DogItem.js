284a8b2a4341bb48289a39daab754fb8c2bcd72e
Willowsmith <willowrosedennis@gmail.com>
2021-11-01 14:06:20 -0700

    added and rearranged database tables and added seeds.

---
+++ models/DogItem.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class DogItem extends Model {}
+
+DogItem.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'dog',
+        key: 'id',
+      },
+    },
+    item_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'item',
+          key: 'id',
+        },
+      },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'dogItem',
+  }
+);
+
+module.exports = DogItem;
