0f424f8cfaf334cc92fff842ecb751268cea6623
Gokuchan <73154359+mshifflette@users.noreply.github.com>
2021-11-03 12:58:56 -0700

    handlebars adoption, adding preview gifs, adding some routing on submit, moving file locations

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -4,7 +4,8 @@ const { User } = require('../models')
 router.get('/', async (req, res) => {
     try{
         res.render('homepage', {
-             logged_in: req.session.logged_in
+             logged_in: req.session.logged_in,
+             style: "style.css"
         })
     }
     catch (err){
@@ -14,7 +15,32 @@ router.get('/', async (req, res) => {
 router.get('/signup', async (req, res) => {
     try{
         res.render('signup', {
-             logged_in: req.session.logged_in
+             logged_in: req.session.logged_in,
+             style: "style.css"
+        })
+    }
+    catch (err){
+        res.status(500).json(err)
+    }
+})
+
+router.get('/dogroom', async (req, res) => {
+    try{
+        res.render('dogroom', {
+             logged_in: req.session.logged_in,
+             style: "style.css"
+        })
+    }
+    catch (err){
+        res.status(500).json(err)
+    }
+})
+
+router.get('/adoption', async (req, res) => {
+    try{
+        res.render('adoption', {
+             logged_in: req.session.logged_in,
+             style: "adoption.css"
         })
     }
     catch (err){
